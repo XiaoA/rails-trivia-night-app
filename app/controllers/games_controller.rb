142efc04f7e44551612dae2ec47899cbcf5589bd
Andrew Buckingham <abuckingham99@gmail.com>
2022-09-01 14:37:28 -0700

    Move `@answer_options` logic from view to controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   def index
+    @answer_options = ("A".."E").to_a
     @question = "In Greek Mythology, who killed Achilles?"
     @answers = ["Hector", "Helen", "Paris", "Pericles"]
     @correct_answer = "Paris"
